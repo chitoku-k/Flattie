d848c109d2dc3a274db91072e4307791880ddf4c
Chitoku <odango@chitoku.jp>
2015-07-28 23:48:27 +0900

    Fix permalink in share-buttons.php

--- src/php/share-buttons.php
+++ src/php/share-buttons.php
@@ -1,25 +1,26 @@
 <aside class="main-content share-buttons-container">
 <?php
 global $title;
+$permalink = ( is_ssl() ? 'https://' : 'http://' ) . filter_input( INPUT_SERVER, 'HTTP_HOST' ) . filter_input( INPUT_SERVER, 'REQUEST_URI' );
 $twitter_url = 'https://twitter.com/share?text=%s&amp;url=%s&amp;related=java_shit';
 $facebook_url = 'https://www.facebook.com/sharer.php?u=%s';
 $google_plus_url = 'https://plus.google.com/share?url=%s';
 $hatena_url = 'https://b.hatena.ne.jp/add?mode=confirm&amp;url=%s';
 $tumblr_url = 'https://www.tumblr.com/share/link?url=%s';
 ?>
-    <a href="<?= sprintf($twitter_url, urlencode( html_entity_decode( $title, ENT_QUOTES, 'UTF-8' ) ), get_permalink() ) ?>" target="_blank" class="share-button twitter-icon">
+    <a href="<?= sprintf($twitter_url, urlencode( html_entity_decode( $title, ENT_QUOTES, 'UTF-8' ) ), $permalink ) ?>" target="_blank" class="share-button twitter-icon">
         <i class="fa fa-twitter"></i>
     </a>
-    <a href="<?= sprintf($facebook_url, get_permalink() ) ?>" target="_blank" class="share-button facebook-icon">
+    <a href="<?= sprintf($facebook_url, $permalink ) ?>" target="_blank" class="share-button facebook-icon">
         <i class="fa fa-facebook"></i>
     </a>
-    <a href="<?= sprintf($google_plus_url, get_permalink() ) ?>" target="_blank" class="share-button google-plus-icon">
+    <a href="<?= sprintf($google_plus_url, $permalink ) ?>" target="_blank" class="share-button google-plus-icon">
         <i class="fa fa-google-plus"></i>
     </a>
-    <a href="<?= sprintf($hatena_url, get_permalink() ) ?>" target="_blank" class="share-button hatena-icon">
+    <a href="<?= sprintf($hatena_url, $permalink ) ?>" target="_blank" class="share-button hatena-icon">
         <i class="fa fa-hatena"></i>
     </a>
-    <a href="<?= sprintf($tumblr_url, urlencode( get_permalink() ) ) ?>" target="_blank" class="share-button tumblr-icon">
+    <a href="<?= sprintf($tumblr_url, urlencode( $permalink ) ) ?>" target="_blank" class="share-button tumblr-icon">
         <i class="fa fa-tumblr"></i>
     </a>
 </aside>
