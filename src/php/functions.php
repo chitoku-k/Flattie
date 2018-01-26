3f5984e058c5bdef0a2a6c3d50fe90990c797225
Chitoku <odango@chitoku.jp>
2018-01-27 02:20:26 +0900

    Remove unnecessary attributes in <script>/<link>

--- src/php/functions.php
+++ src/php/functions.php
@@ -59,6 +59,13 @@ remove_filter( 'template_redirect', 'redirect_canonical' );
 add_filter( 'style_loader_src', 'remove_ver_query', 9999 );
 add_filter( 'script_loader_src', 'remove_ver_query', 9999 );
 
+add_filter( 'style_loader_tag', function ( $tag ) {
+    return str_replace( "type='text/css' ", '', $tag );
+} );
+add_filter( 'script_loader_tag', function ( $tag ) {
+    return str_replace( "type='text/javascript'", 'defer', $tag );
+} );
+
 add_filter( 'the_content_more_link', function ( $output, $more_link_text ) {
     return '<div class="read-more-container"><a href="' . get_permalink() . '" class="read-more-button">' . $more_link_text . '</a></div>';
 }, 10, 2 );
