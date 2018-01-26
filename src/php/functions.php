652ad58a0f3e4854bae3355fa5573807779ebd22
Chitoku <odango@chitoku.jp>
2018-01-27 02:34:37 +0900

    Use built-in style/scripts loader of WordPress

--- src/php/functions.php
+++ src/php/functions.php
@@ -46,6 +46,11 @@ add_action( 'get_header', function () {
     }
 }, 100 );
 
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'flattie-style', get_stylesheet_uri() );
+    wp_enqueue_script( 'flattie-script', get_template_directory_uri() . '/js/main.js', array( 'jquery' ) );
+} );
+
 add_action( 'widgets_init', function () {
     register_widget( Same_Category_Posts_Widget::class );
     register_widget( Share_Button_Widget::class );
