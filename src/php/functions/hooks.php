e5ec1956cefe254ffb7cd40c86fdc0d30cb03400
Chitoku <odango@chitoku.jp>
2018-01-28 02:10:22 +0900

    Update WordPress hook for script elements

--- src/php/functions/hooks.php
+++ src/php/functions/hooks.php
@@ -1,22 +1,63 @@
 <?php
-// Hooks relating to scripts
-$wp_scripts = new Flattie_Scripts();
+/**
+ * Hooks relating to scripts loaded by WordPress
+ *
+ * The WP_Scripts class outputs script elements with type attribute attached.
+ * In HTML5, it is advised to omit type attribute and replacing the functions
+ * `print_extra_script` and `print_inline_script` did the trick.
+ */
+$wp_scripts = new class extends WP_Scripts {
+    public function print_extra_script( $handle, $echo = true ) {
+        $output = parent::print_extra_script( $handle, false );
 
-// Hooks relating to All in One SEO Pack
-if ( class_exists( 'All_in_One_SEO_Pack' ) ) {
+        if ( ! $output ) {
+            return;
+        }
+        if ( ! $echo ) {
+            return $output;
+        }
+
+        echo "<script>\n{$output}\n</script>\n";
+        return true;
+    }
+
+    public function print_inline_script( $handle, $position = 'after', $echo = true ) {
+        $output = parent::print_inline_script( $handle, $position, false );
+
+        if ( ! $output ) {
+            return false;
+        }
+        if ( $echo ) {
+            echo "<script>\n{$output}\n</script>\n";
+        }
+
+        return $output;
+    }
+};
+
+/**
+ * Hooks relating to scripts loaded by All in One SEO Pack
+ *
+ * The All in One SEO Pack outputs script elements with type attribute attached.
+ * In HTML5, it is advised to emit type attribute and replacing the function
+ * `universal_analytics` in aioseop_google_analytics did the trick.
+ */
+if ( class_exists( 'aioseop_google_analytics' ) ) {
     add_filter( 'init', function () use ( $aiosp ) {
+        // Remove the action that prints Google Analytics
         remove_action( 'aioseop_modules_wp_head', [ $aiosp, 'aiosp_google_analytics' ] );
 
-        $aiosp = new class extends All_in_One_SEO_Pack {
-            public function aiosp_google_analytics() {
+        // Add an action replacing the type attribute
+        add_action(
+            'wp_head',
+            function () {
                 new class extends aioseop_google_analytics {
                     public function universal_analytics() {
                         return str_replace( ' type="text/javascript" ', '', parent::universal_analytics() ) . "\n";
                     }
                 };
-            }
-        };
-
-        add_action( 'wp_head', [ $aiosp, 'aiosp_google_analytics' ], apply_filters( 'aioseop_wp_head_priority', 1 ) );
+            },
+            apply_filters( 'aioseop_wp_head_priority', 1 )
+        );
     } );
 }
