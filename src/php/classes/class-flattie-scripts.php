26db2200532a5d04ed1e0c60362298eeb7359265
Chitoku <odango@chitoku.jp>
2018-01-28 00:43:58 +0900

    Stop empty script output

--- src/php/classes/class-flattie-scripts.php
+++ src/php/classes/class-flattie-scripts.php
@@ -3,6 +3,9 @@ class Flattie_Scripts extends WP_Scripts {
     public function print_extra_script( $handle, $echo = true ) {
         $output = parent::print_extra_script( $handle, false );
 
+        if ( ! $output ) {
+            return;
+        }
         if ( ! $echo ) {
             return $output;
         }
@@ -14,6 +17,9 @@ class Flattie_Scripts extends WP_Scripts {
     public function print_inline_script( $handle, $position = 'after', $echo = true ) {
         $output = parent::print_inline_script( $handle, $position, false );
 
+        if ( ! $output ) {
+            return false;
+        }
         if ( $echo ) {
             echo "<script>\n{$output}\n</script>\n";
         }
