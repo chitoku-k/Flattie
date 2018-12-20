09720072058a0be900bd297b84a1ad11e567422d
Chitoku <odango@chitoku.jp>
2018-12-20 12:10:52 +0900

    Remove Contact Form 7 from all pages but /mail

--- src/php/functions/actions.php
+++ src/php/functions/actions.php
@@ -16,6 +16,18 @@ add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_script( 'flattie-script', get_template_directory_uri() . 'js/main.js' );
 } );
 
+add_action( 'wpcf7_enqueue_styles', function () {
+    if ( ! is_page( 'mail' ) ) {
+        wp_deregister_style( 'contact-form-7' );
+    }
+} );
+
+add_action( 'wpcf7_enqueue_scripts', function () {
+    if ( ! is_page( 'mail' ) ) {
+        wp_deregister_script( 'jquery-form' );
+    }
+} );
+
 add_action( 'widgets_init', function () {
     register_widget( Same_Category_Posts_Widget::class );
     register_widget( Share_Button_Widget::class );
